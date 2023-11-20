fca356b4319a87ec1bcd52b7ad4f63e3bdbc7d71
rajadurainataraj <rajadurainatraj@gmail.com>
2023-11-20 17:46:29 +0530

    package installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { FaBeer } from "react-icons/fa"
+import styled from "styled-components"
+const Container = styled.h1`
+  background-color: red;
+`
 function App() {
   return (
     <>
-      <h1>Vijayakumar</h1>
+      <Container>
+        Vijayakumar <FaBeer />
+      </Container>
     </>
   )
 }
